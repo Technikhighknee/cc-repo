20c8f045d34de034780ac803a43e39a062c1db5a
Jason Posch <poschjason@gmail.com>
2026-05-16 13:59:57 +0200

    fix: remove unnecessary space in shell.execute call in download script

--- bin/download.lua
+++ bin/download.lua
@@ -8,4 +8,4 @@ local source = arg[1]
 local destination = arg[2] or string.match(source, "[^/]+$"); -- some_path/some_script.lua -> some_script.lua
 local url = GITHUB_REPOSITORY_URL_BASE .. source
 
-shell.execute("wget",  url, destination)
+shell.execute("wget", url, destination)
