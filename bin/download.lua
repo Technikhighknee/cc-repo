3d97a10c852d48f20d70c6e9fb8fa3227c922df5
Jason Posch <poschjason@gmail.com>
2026-05-16 10:05:20 +0200

    refactor: update download script to improve argument handling and use shell.execute (which is a bugfix)

--- bin/download.lua
+++ bin/download.lua
@@ -1,11 +1,11 @@
 local GITHUB_REPOSITORY_URL_BASE = "https://raw.githubusercontent.com/Technikhighknee/cc-repo/refs/heads/main/"
-
-local path = arg[1]
-local file_name = arg[2] or string.match(path, "[^/]+$"); -- some_path/some_script.lua -> some_script.lua
-
-if not file_name then
-    print("Usage: download <path> [file_name]")
+if not arg[1] then
+    print("Usage: download <source> [destination]")
     return
 end
 
-os.execute("wget " .. GITHUB_REPOSITORY_URL_BASE .. path .. " " .. file_name)
+local source = arg[1]
+local destination = arg[2] or string.match(source, "[^/]+$"); -- some_path/some_script.lua -> some_script.lua
+local url = GITHUB_REPOSITORY_URL_BASE .. source
+
+shell.execute("wget",  url, destination)
