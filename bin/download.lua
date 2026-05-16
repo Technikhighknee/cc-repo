9e66db3be35cc1a659f2eb36564bec39eacaff9b
Jason Posch <poschjason@gmail.com>
2026-05-16 14:06:54 +0200

    taste: moved constant GITHUB_REPOSITORY_URL_BASE under guard clause.

--- bin/download.lua
+++ bin/download.lua
@@ -1,9 +1,9 @@
-local GITHUB_REPOSITORY_URL_BASE = "https://raw.githubusercontent.com/Technikhighknee/cc-repo/refs/heads/main/"
 if not arg[1] then
     print("Usage: download <source> [destination]")
     return
 end
 
+local GITHUB_REPOSITORY_URL_BASE = "https://raw.githubusercontent.com/Technikhighknee/cc-repo/refs/heads/main/"
 local source = arg[1]
 local destination = arg[2] or string.match(source, "[^/]+$"); -- some_path/some_script.lua -> some_script.lua
 local url = GITHUB_REPOSITORY_URL_BASE .. source
