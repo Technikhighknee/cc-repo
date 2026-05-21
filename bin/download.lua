939425823224052260eb1f0a0080f5ba25978f3b
Jason Posch <poschjason@gmail.com>
2026-05-22 00:02:19 +0200

    feat: add check to delete existing destination file before download

--- bin/download.lua
+++ bin/download.lua
@@ -8,4 +8,8 @@ local source = arg[1]
 local destination = arg[2] or string.match(source, "[^/]+$"); -- some_path/some_script.lua -> some_script.lua
 local url = GITHUB_REPOSITORY_URL_BASE .. source
 
+if (fs.exists(destination)) then
+    fs.delete(destination)
+end
+
 shell.execute("wget", url, destination)
